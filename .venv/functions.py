585397e62fcd92ff1777675a834be842d2d4dbd0
Naitik <naitikpahwa11@gmail.com>
2024-04-07 06:21:23 +0530

    Importing os (2)

--- .venv/functions.py
+++ .venv/functions.py
@@ -1,21 +1,10 @@
-import os
-
-if not os.path.exists('todos.txt'):
-    with open('todos.txt' , 'w') as file:
-        pass
-
 FILEPATH = "todos.txt"
-
 def get_todos(filepath = FILEPATH):                   # Here filepath is the parameter and "todos.txt" is
     # Doc-string                                      # the default argument value if no value is given.
     '''Read a text file and returns a list
     of to-do items.'''
-    filepath = os.path.join(os.getcwd(), "todos.txt")
-    # os.getcwd() to get the current working directory of the application.
-    # os.path.join() to construct a relative path to "todos.txt" from the root directory of the application.
-
     with open(filepath, 'r') as file_local:
-        todos_local = [line.strip() for line in file_local.readlines()]
+        todos_local = file_local.readlines()
     return todos_local
 
 def write_todos(todos_arg, filepath = FILEPATH):
